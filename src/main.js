2c5854cdb2ac2cd2bd6ef4dd0d5633f822f25d9e
chethak-may18 <chethak.vishwanath@gmail.com>
2026-03-25 20:41:32 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log("Sprintly started");
+import { auth, provider, signInWithPopup } from "./firebase.js";
+
+const loginBtn = document.getElementById("login-btn");
+const userBox = document.getElementById("user-box");
+
+loginBtn.addEventListener("click", async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+
+    // Show user info
+    userBox.innerHTML = `
+      <h2>Welcome ${user.displayName}</h2>
+      <p>${user.email}</p>
+      <img src="${user.photoURL}" width="80" style="border-radius:50%" />
+    `;
+
+  } catch (error) {
+    console.error(error);
+    alert("Login failed");
+  }
+});
